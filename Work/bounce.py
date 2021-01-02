7761ba379935cab3d718ce257ace93106ac54139
Raunak Mundada <raunakm90@gmail.com>
2021-01-01 19:36:10 -0600

    01_Introduction Exercises

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+
+h = 100
+i = 0
+while i < 10:
+    h = h*(3/5)
+    i += 1
+    print(round(h, 4))
---
+++ Work/vscode_explore.py
@@ -0,0 +1,11 @@
+import urllib.request
+from xml.etree.ElementTree import parse
+
+help(abs)
+
+u = urllib.request.urlopen(
+    'http://ctabustracker.com/bustime/map/getStopPredictions.jsp?stop=14791&route=22')
+
+doc = parse(u)
+for pt in doc.findall('.//pt'):
+    print(pt.text)
